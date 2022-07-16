ea8eac8dcbbb00c82d68c93487549873a159f2bf
Robert Durica <r.durica@gmail.com>
2022-07-16 21:16:01 +0200

    Initial Commit

---
+++ TodoList-MVVM/TodoList_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList_MVVMApp.swift
+//  TodoList-MVVM
+//
+//  Created by Robert Ďurica on 16.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoList_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
